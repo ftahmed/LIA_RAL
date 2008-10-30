ec3b4cf1ede04f74446f8f8a1f7bad4c9b8f2802
admin <admin@1290fa5e-fd49-494a-a208-9d5a245c2233>
2008-10-30 13:31:05 +0000

    Modifs diverses / bug / Confort

--- install.sh
+++ install.sh
@@ -3,9 +3,6 @@
 #DIALOG=gdialog
 DIALOG=dialog
 
-# Graphical install to appear soon
-#$DIALOG --title "Step 1" --backtitle "LIA_RAL Installation" --yesno "Welcome to LIARAL installation, please proceed to continue" 10 50
-
 ##### Copier les données de config.txt.ex dans le config.txt
 perl -pe 'my $dir=`pwd`;s/(LIA_RAL_TOPDIR=)/\1$dir/g' config.txt.ex > config.txt
 
