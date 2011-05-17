2f59ce095030f67b62770e6cce3812adff475276
clevy <clevy@1290fa5e-fd49-494a-a208-9d5a245c2233>
2011-05-17 05:40:50 +0000

    init

---
+++ branches/2.0/LIA_SpkDet/EstimateDMatrix/include/EstimateDMatrix.h
@@ -0,0 +1,13 @@
+#if !defined(ALIZE_EstimateDMatrix_h)
+#define ALIZE_EstimateDMatrix_h
+
+#include <alize.h>
+#include "liatools.h"
+
+using namespace alize;
+using namespace std;
+
+int EstimateDMatrix(Config &);
+
+#endif 
+
