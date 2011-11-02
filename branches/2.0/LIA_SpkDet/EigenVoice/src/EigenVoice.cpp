b2c5a8812283def6ab8c681182caf3328204c1ef
alarcher <alarcher@1290fa5e-fd49-494a-a208-9d5a245c2233>
2011-11-02 09:10:26 +0000

    Update ALarcher

--- branches/2.0/LIA_SpkDet/EigenVoice/src/EigenVoice.cpp
+++ branches/2.0/LIA_SpkDet/EigenVoice/src/EigenVoice.cpp
@@ -63,30 +63,27 @@ Jean-Francois Bonastre [jean-francois.bonastre@univ-avignon.fr]
 #include <cmath>
 #include <liatools.h>
 #include "EigenVoice.h"
-#include "AccumulateJFAStat.h"
 
 using namespace std;
 using namespace alize;
 
-
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
 int EigenVoice(Config & config){
-	
-	cerr<<"entering Eigenvoices"<<endl;
 
 	//Read the NDX file
 	String ndxFilename = config.getParam("ndxFilename");
 	
 	//Create and initialise the accumulator
-	JFAAcc jfaAcc(ndxFilename, config);
+	JFAAcc jfaAcc(ndxFilename, config,"EigenVoice");
 
 	//Option used to check the Likelihood at each iteration
 	bool _checkLLK = false;
 	if (config.existsParam("checkLLK")) _checkLLK= config.getParam("checkLLK").toBool();
-	
+	else if (verboseLevel >=1) _checkLLK= true;
+
 	//Statistics
 	if((config.existsParam("loadAccs")) && config.getParam("loadAccs").toBool()){	//load pre-computed statistics
-		cerr<<"load accumulators"<<endl;
+		cout<<"	(EigenVoice)Load Accumulators"<<endl;
 		jfaAcc.loadN(config);
 		jfaAcc.loadN_h(config);
 		jfaAcc.loadF_X(config);
@@ -96,7 +93,7 @@ int EigenVoice(Config & config){
 		jfaAcc.computeAndAccumulateJFAStat(config);
 		jfaAcc.saveAccs(config);
 	}
-	
+
 	//Initialise the EV Matrix
 	bool loadInitEigenVoiceMatrix = false;
 	if(config.existsParam("loadInitEigenVoiceMatrix")) loadInitEigenVoiceMatrix = config.getParam("loadInitEigenVoiceMatrix").toBool();
@@ -121,7 +118,6 @@ int EigenVoice(Config & config){
 	for(unsigned long it=0; it<nbIt; it++){
 		
 		cout<<"	(EigenVoices) --------- start iteration "<<it<<" --------"<<endl;
-		if (_checkLLK) jfaAcc.verifyEMLK(config);
 		
 		//On calcules les matrices vEvT
 		jfaAcc.estimateVEVT(config);
@@ -134,17 +130,24 @@ int EigenVoice(Config & config){
 		
 		//On soustrait pour chaque locuteur la composante canal de chaque session
 		jfaAcc.substractUX(config);
-		
+
 		//On update Y pour tous les locuteurs
 		jfaAcc.estimateYandV(config);
-		
+
 		//Update _V
 		jfaAcc.updateVestimate();
 
+		if (_checkLLK) jfaAcc.verifyEMLK(config);
+
+		//If the option is on, orthonormalize the matrix V
+		if(config.getParam("orthonormalizeV").toBool()){
+			if(verboseLevel > 0) cerr<<"Orthonormalize EV matrix"<<endl;
+			jfaAcc.orthonormalizeV();
+		}
+
 		//Reinitialise the accumulators
-		jfaAcc.resetTmpAcc();
 		jfaAcc.restoreAccs();
-		
+
 		//Save the V matrix at the end of the iteration
 		bool saveAllEVMatrices = false;
 		if(config.existsParam("saveAllEVMatrices")) saveAllEVMatrices=config.getParam("saveAllEVMatrices").toBool();
@@ -161,4 +164,127 @@ int EigenVoice(Config & config){
 
 return 0;
 }
+
+
+
+
+
+
+
+
+
+int IVector(Config & config){
+
+	//Read the NDX file
+	String ndxFilename = config.getParam("ndxFilename");
+	
+	//Create and initialise the accumulator
+	JFAAcc jfaAcc(ndxFilename, config,"iv");
+
+	//Option used to check the Likelihood at each iteration
+	bool _checkLLK = false;
+	if (config.existsParam("checkLLK")) _checkLLK= config.getParam("checkLLK").toBool();
+	else if (verboseLevel >=1) _checkLLK= true;
+
+	//Statistics
+	if((config.existsParam("loadAccs")) && config.getParam("loadAccs").toBool()){	//load pre-computed statistics
+		cout<<"	(EigenVoice)Load Accumulators"<<endl;
+		jfaAcc.loadN(config);
+		jfaAcc.loadN_h(config);
+		jfaAcc.loadF_X(config);
+		jfaAcc.loadF_X_h(config);
+	}
+	else{															//Compute statistics if they don't exists
+		jfaAcc.computeAndAccumulateJFAStat(config);
+		jfaAcc.saveAccs(config);
+	}
+
+	//Initialise the EV Matrix
+	bool loadInitEigenVoiceMatrix = false;
+	if(config.existsParam("loadInitEigenVoiceMatrix")) loadInitEigenVoiceMatrix = config.getParam("loadInitEigenVoiceMatrix").toBool();
+	if(loadInitEigenVoiceMatrix){	//Load the EV matrix when existing
+		jfaAcc.loadEV(config.getParam("initEigenVoiceMatrix"), config);
+	}
+	else{	//Initialise the EV matrix randomly if does not exists
+		jfaAcc.initEV(config);
+	}
+	
+	//Save the initial V matrix to be able restart the process with the same initialisation
+	if(config.existsParam("saveInitEigenVoiceMatrix") && config.getParam("saveInitEigenVoiceMatrix").toBool()){
+		String initV = config.getParam("eigenVoiceMatrix")+"_init";
+		jfaAcc.saveV(initV, config);
+		cout<<"	(EigenVoice) Save the initial EigenVoice Matrix in "<<initV<<endl;
+	}
+
+	//iteratively retrain the EV matrix
+	unsigned long nbIt = config.getParam("nbIt").toULong();
+	
+	jfaAcc.storeAccs();
+	for(unsigned long it=0; it<nbIt; it++){
+		
+		cout<<"	(EigenVoices) --------- start iteration "<<it<<" --------"<<endl;
+		
+		//On calcules les matrices vEvT
+		jfaAcc.estimateVEVT(config);
+
+		//On calcules les matrices L et on les inverse (on integre la boucle sur tous les locuteurs dans cette fonction)
+		jfaAcc.estimateAndInverseL_EV(config);
+
+		//On soustrait les statistiques du locuteur (M+DZ)*Ns pour chaque locuteur
+		jfaAcc.substractMplusDZ(config);
+		
+		//On soustrait pour chaque locuteur la composante canal de chaque session
+		jfaAcc.substractUX(config);
+
+		//On update Y pour tous les locuteurs
+		jfaAcc.estimateYandV(config);
+
+		//Update _V
+		jfaAcc.updateVestimate();
+
+		if (_checkLLK) jfaAcc.verifyEMLK(config);
+
+		//If the option is on, orthonormalize the matrix V
+		if(config.getParam("orthonormalizeV").toBool()){
+			if(verboseLevel > 0) cerr<<"Orthonormalize EV matrix"<<endl;
+			jfaAcc.orthonormalizeV();
+		}
+
+		//Reinitialise the accumulators
+		jfaAcc.resetTmpAcc("iv");
+		jfaAcc.restoreAccs();
+		//Save the V matrix at the end of the iteration
+		bool saveAllEVMatrices = false;
+		if(config.existsParam("saveAllEVMatrices")) saveAllEVMatrices=config.getParam("saveAllEVMatrices").toBool();
+		if(saveAllEVMatrices){
+			String s;
+			String output = config.getParam("eigenVoiceMatrix") + s.valueOf(it);
+			jfaAcc.saveV(output, config);
+		}
+	}
+
+	cout<<"	(EigenVoices) --------- save EigenVoices Matrix --------"<<endl;
+	jfaAcc.saveV(config.getParam("eigenVoiceMatrix"), config);
+	cout<<"	(EigenVoices) --------- end of process --------"<<endl;
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #endif 
